49c88b8519f5d015f3af47e97996c4cda918a498
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-15 14:19:42 +0600

    Update 2023_02_14_164949_create_posts_table.php

--- laravel-ui/database/migrations/2023_02_14_164949_create_posts_table.php
+++ laravel-ui/database/migrations/2023_02_14_164949_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('name');
             $table->string('email')->unique();;
             $table->string('description')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
